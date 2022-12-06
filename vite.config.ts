cea34efd3f5df3ebad176b915c16df9f17a44e47
zhangbowy <1751170889@qq.com>
2022-12-06 17:31:40 +0800

    build: 增加打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import {resolve} from 'path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-    server: {
-        port: 3201
+    build: {
+        lib: {
+            entry: resolve(__dirname, './packages/index.ts'),
+            name: 'puUI',
+            fileName: 'pu-ui'
+        },
+        rollupOptions: {
+            external: ['vue'],
+            output: {
+                globals: {
+                    vue: 'Vue'
+                }
+            }
+        }
     },
-    plugins: [
-        vue()
-    ]
+    plugins: [vue(), dts({include: './packages'})]
 })
