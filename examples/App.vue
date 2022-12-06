5319db5c27ffb2e9c655f9c599d314285830ca6d
zhangbowy <1751170889@qq.com>
2022-12-06 17:54:04 +0800

    feat: 增加本地开发测试示例

---
+++ examples/App.vue
@@ -0,0 +1,12 @@
+<template>
+
+</template>
+
+<script setup lang="ts">
+import ChooseUser from "./../packages/chooseuserBak"; // 新增
+ChooseUser({})
+</script>
+
+<style scoped>
+
+</style>
---
+++ examples/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from "vue";
+// import "./style.css";
+import 'ant-design-vue/dist/antd.css';
+
+import App from "./App.vue";
+
+const app = createApp(App);
+// app.use(ChooseUser); // 新增
+app.mount("#app");
